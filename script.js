d6f47e9bc593b79fcd2571d39114a8eea01a2781
RJ Ocampo <rjqocampo@gmail.com>
2023-07-28 22:35:25 +0800

    Add event listeners for numbers and operators

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
-let aInput = null;
-let bInput = null;
+const buttonsNumber = document.querySelectorAll('.button-number');
+const buttonsOperator = document.querySelectorAll('.button-operator');
+const display = document.querySelector('#display');
+
+let a = null;
+let b = null;
 let operator = '';
-let currentValue = 0;
+let displayValue = '';
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a -b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+buttonsNumber.forEach(button => {
+    button.addEventListener('click', (event) => {
+        displayValue = displayValue + event.target.textContent;
+        showDisplay()
+    });
+})
 
-function operate (a, b, operator) {
-    if (operator === '+') {return add(a, b)}
-    else if (operator === '-') {return subtract(a, b)}
-    else if (operator === '*') {return multiply(a, b)}
-    else if (operator === '/') {return divide(a, b)};
+buttonsOperator.forEach(button => {
+    button.addEventListener('click', (event) => {
+        getDisplayValue();
+        clearDisplay();
+        assignAB();
+        operate();
+        assignOperator(event);
+    });
+})
+
+function operate () {
+    displayValue = '';
+    if (a !== null & b !== null) {
+        if (operator === '+') {
+            console.log('+');
+            let value = add(a, b);
+            b = value;
+            display.textContent = value;
+            console.log(value);
+        } else if (operator === '-') {
+            console.log('-');
+            let value = subtract(a, b);
+            b = value;
+            display.textContent = value;
+            console.log(value);
+        }
+        // else if (operator === '*') {display.textContent = multiply(a, b)}
+        // else if (operator === '/') {display.textContent = divide(a, b)};
+    }
 }
 
+function assignOperator(event) {
+    operator = event.target.textContent;
+}
+
+function getDisplayValue() {
+    displayValue = parseInt(display.textContent);
+}
+
+function assignAB() {
+    a = b;
+    b = displayValue;
+}
+
+function clearDisplay() {
+    display.textContent = '';
+}
+
+function displayNumber(event) {
+    if (display.textContent === '0') {
+        display.textContent = '';
+    }
+    display.textContent += event.target.textContent;
+}
+
+function showDisplay() {
+    display.textContent = displayValue;
+}
+
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 /**
+after operator, display should clear because getting wrong value for b
+
 
  */
