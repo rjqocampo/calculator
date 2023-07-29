8983ec9c0fc37352097fbcc8e5c44f936ecb5dc8
RJ Ocampo <rjqocampo@gmail.com>
2023-07-29 20:49:38 +0800

    Add toggle negative feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttonsNumber = document.querySelectorAll('.button-number');
 const buttonsOperator = document.querySelectorAll('.button-operator');
 const buttonClear = document.querySelector('#clear');
-const buttonNegative = document.querySelector('#button-negative');
+const buttonNegative = document.querySelector('#negative')
 const displayNumber = document.querySelector('#display-number');
 const displayOperator = document.querySelector('#display-operator');
 
@@ -15,6 +15,8 @@ buttonsNumber.forEach(button => {
     button.addEventListener('click', (event) => {
         if (displayNumber.textContent === '0') {
             displayValue = '';
+        } else if (displayNumber.textContent === '-0') {
+            displayValue = '-'
         }
         displayValue = displayValue + event.target.textContent;
         showDisplay()
@@ -40,21 +42,22 @@ function operate () {
     if (a !== null & b !== null) {
         if (operator === '+') {
             let value = add(a, b);
+            // a = b; ----------- correct logic, but necessary?
             b = value; 
-            displayNumber.textContent = value;
+            displayNumber.textContent = b;
         } else if (operator === '-') {
             let value = subtract(a, b);
             b = value;
-            displayNumber.textContent = value;
+            displayNumber.textContent = b;
         } else if (operator === '*') {
             let value = multiply(a, b);
             b = value;
-            displayNumber.textContent = value;
+            displayNumber.textContent = b;
         } else if (operator === '/') {
             let value = divide(a, b);
             b = value;
-            displayNumber.textContent = value;
-        };
+            displayNumber.textContent = b;
+        }  
     };
 };
 
@@ -76,11 +79,42 @@ function clearDisplay() {
 }
 
 function showOperator() {
-    displayOperator.textContent = `${operator}`;
+    displayOperator.textContent = operator;
 }
 
 function showDisplay() {
     displayNumber.textContent = displayValue;
+}
+
+function toggleNegative() { 
+    if (displayNumber.textContent === '0') { return };
+        if (displayNumber.textContent.includes('-')) {
+            console.log('null slice')
+            displayNumber.textContent = displayNumber.textContent.slice(1);
+        } else {
+            console.log('null negative')
+            displayNumber.textContent = '-' + displayNumber.textContent;
+
+    // } else if (a !== null && b !== null) {
+    //     if (displayNumber.textContent.includes('-')) {
+    //         console.log('b slice');
+
+    //         displayNumber.textContent = displayNumber.textContent.slice(1);
+
+    //         // b = Math.abs(b);
+    //         // displayNumber.textContent = b;
+    //         // displayValue = b;
+    //     } else {
+    //         console.log('b negative')
+
+    //         displayNumber.textContent = '-' + displayNumber.textContent;
+
+    //         // b = -b;
+    //         // displayNumber.textContent = b;
+    //         // displayValue = b;
+    //     }
+    
+    }
 }
 
 function clear() {
@@ -92,15 +126,6 @@ function clear() {
     showOperator();
 }
 
-function toggleNegative() {
-    if (displayValue.includes('-')) {
-        displayValue = displayValue.slice(1);
-    } else {
-        displayValue = '-' + displayValue 
-    };
-    showDisplay();
-}
-
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -109,11 +134,24 @@ const divide = (a, b) => a / b;
 showDisplay();
 
 /**
-    +/- if > 0, turn negative. Vice versa
     % is n / 100
     use slice(str.length - 1) for delete function
     minor bug when doing + then equals. number operates on itself
     minor bug, auto rounds to floor if there's decimals
     add decimals. should only use once on a string
     add keyboard support
+ */
+
+/**
+    after an operation, when a and b are not null. toggle nega still refers to b instead of display
+
+    if display.textContent is '0', don't do anything
+    if a is null or b is null,
+        if includes '-', slice(1)
+        else if '-' + displayNumber.textContent
+    if a not null and b not null,
+        if includes '-', slice(1)
+        else if '-' + b, might need to parse? 
+
+    showDisplay();
  */
